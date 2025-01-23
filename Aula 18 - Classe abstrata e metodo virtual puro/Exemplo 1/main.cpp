ff235452c7f032a254c200034df2c5633d2152c9
Bruno S. Faical <bsfaical@alumni.usp.br>
2025-01-23 18:22:59 -0300

    add: exemplo da aula 18

---
+++ Aula 18 - Classe abstrata e metodo virtual puro/Exemplo 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "carro.hpp"
+
+using namespace std;
+
+int main(){
+    // Veiculo *v1 = new Veiculo();
+    Veiculo *v2 = new Carro();
+
+    // v1->exibe();
+    v2->exibe();
+}
---
+++ Aula 18 - Classe abstrata e metodo virtual puro/Exemplo 1/veiculo.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+class Veiculo{
+    public:
+    virtual void exibe()=0;
+
+};
